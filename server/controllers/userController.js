6176c36eb720de21502d5b2ca0118759eb4088f1
Sean Sollestre <sean.sollestre@gmail.com>
2024-03-09 10:09:35 -0800

    Add mongodb CRUD operations to endpoints

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,27 +4,85 @@ const ObjectId = mongoose.Types.ObjectId;
 
 // Create user
 const createUser = async (req, res) => {
-  res.json({ msg: "create" });
+  let { email, username, password, points, questIds } = req.body;
+
+  try {
+    const user = await User.create({
+      email,
+      username,
+      password,
+      points,
+      questIds,
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 // Read user
 const readUser = async (req, res) => {
-  res.json({ msg: "read" });
+  const { id } = req.query;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "Invalid id" });
+  }
+
+  const user = await User.findById(id);
+
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+
+  res.status(200).json(user);
 };
 
 // Read all users
 const readAllUsers = async (req, res) => {
-  res.json({ msg: "read all" });
+  try {
+    const users = await User.find({}).sort({ createdAt: -1 });
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 // Update user
 const updateUser = async (req, res) => {
-  res.json({ msg: "update" });
+  const { id, ...data } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "invalid id" });
+  }
+
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: ObjectId.createFromHexString(id) },
+      { ...data },
+      { runValidators: true }
+    );
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 // Delete user
 const deleteUser = async (req, res) => {
-  res.json({ msg: "delete" });
+  const { id } = req.query;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "Invalid id" });
+  }
+
+  const user = await User.findOneAndDelete({
+    _id: ObjectId.createFromHexString(id),
+  });
+
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+
+  res.status(200).json(user);
 };
 
 module.exports = {
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
   username: {
     type: String,
     required: true,
