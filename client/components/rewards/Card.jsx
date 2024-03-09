23e225cf161cdb4afb27c679ac82d79c1e5ef729
Sean Sollestre <sean.sollestre@gmail.com>
2024-03-09 12:57:43 -0800

    Add initial rewards page

---
+++ client/components/rewards/Card.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  Button,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+
+const Card = ({ name, description, price }) => (
+  <View style={styles.card}>
+    <View style={styles.row}>
+      {/* Image container */}
+      <View style={[styles.column, { width: "25%" }]}>
+        <View style={styles.imgContainer}>
+          <Image
+            source={require("./testReward.png")} // Specify the path to the local image
+            style={styles.image}
+          />
+        </View>
+      </View>
+      {/* Details container */}
+      <View style={([styles.column], { width: "75%" })}>
+        <Text style={styles.name}>{name}</Text>
+        <Text>{description}</Text>
+        <View style={[styles.row, { justifyContent: "space-between" }]}>
+          <View style={[styles.column]}>
+            <Text>Price: {price} points</Text>
+          </View>
+          {/* Purchase button */}
+          <View style={[styles.column]}>
+            <TouchableOpacity style={styles.button}>
+              <Text>Buy</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </View>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  card: {
+    width: 350,
+    backgroundColor: "white",
+    padding: 10,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  name: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  row: {
+    flexDirection: "row",
+  },
+  column: {
+    margin: 5,
+    paddingRight: 10,
+    // borderColor: "black",
+    // borderWidth: 1,
+  },
+  image: {
+    width: 70,
+    height: 70,
+    resizeMode: "cover", // Set the image resizing mode
+  },
+  imgContainer: {},
+  button: {
+    backgroundColor: "lightgray",
+    borderRadius: 5,
+    height: 30,
+    width: 60,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default Card;
--- client/components/rewards/RewardsScreen.jsx
+++ client/components/rewards/RewardsScreen.jsx
@@ -1,17 +1,106 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import { useEffect } from "react";
+import Card from "./Card";
+
+const testRewardData = [
+  {
+    name: "Test Reward",
+    description: "Some Yummy Nummies or smt",
+    price: 25,
+  },
+  {
+    name: "Test Reward",
+    description: "Some Yummy Nummies or smt",
+    price: 25,
+  },
+  {
+    name: "Test Reward",
+    description: "Some Yummy Nummies or smt",
+    price: 25,
+  },
+  {
+    name: "Test Reward",
+    description: "Some Yummy Nummies or smt",
+    price: 25,
+  },
+  {
+    name: "Test Reward",
+    description: "Some Yummy Nummies or smt",
+    price: 25,
+  },
+  {
+    name: "Test Reward",
+    description: "Some Yummy Nummies or smt",
+    price: 25,
+  },
+  {
+    name: "Test Reward",
+    description: "Some Yummy Nummies or smt",
+    price: 25,
+  },
+];
 
 const RewardsScreen = () => {
   return (
-    <View>
-      <Text>Rewards Screen</Text>
-      <Text>Rewards Screen</Text>
-      <Text>Rewards Screen</Text>
-      <Text>Rewards Screen</Text>
-      <Text>Rewards Screen</Text>
-      <Text>Rewards Screen</Text>
-    </View>
+    <>
+      {/* Rewards Description */}
+      <View style={styles.rewardDescription}>
+        <Text style={styles.descriptionTitle}>Rewards</Text>
+        <Text style={styles.descriptionText}>
+          Redeem your points for cool prizes!
+        </Text>
+      </View>
+
+      {/* Rewards List */}
+      <View style={styles.container}>
+        <FlatList
+          data={testRewardData}
+          renderItem={({ item, index }) => (
+            <Card
+              key={index.toString()}
+              name={item.name}
+              description={item.description}
+              price={item.price}
+            />
+          )}
+          keyExtractor={(_item, index) => index.toString()}
+        />
+      </View>
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  rewardDescription: {
+    width: "90%",
+    backgroundColor: "white",
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  descriptionTitle: {
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+  descriptionText: {
+    fontSize: 20,
+  },
+});
 
 export default RewardsScreen;
