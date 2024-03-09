3e75df8b29431b4bf8dfee2556e8edb59e7a04de
Carol Li <lizhi.2496@gmail.com>
2024-03-09 09:48:16 -0800

    added questController and questModel

---
+++ server/models/questModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const questSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  points: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  categoryId: {
+    type: [String],
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Quest", questSchema);
--- server/routers/questRouter.js
+++ server/routers/questRouter.js
@@ -1,29 +1,26 @@
 const express = require("express");
 const questRouter = express.Router();
+const {
+  createQuest,
+  readQuest,
+  readAllQuests,
+  updateQuest,
+  deleteQuest,
+} = require("../controllers/questController");
 
 // Create 
-  questRouter.post("/create", (req, res) => {
-  res.json({ msg: "create" });
-});
+  questRouter.post("/create", createQuest);
 
 // Read 
-  questRouter.get("/read", (req, res) => {
-  res.json({ msg: "read" });
-});
+  questRouter.get("/read", readQuest);
 
 // Read All 
-  questRouter.get("/read/all", (req, res) => {
-  res.json({ msg: "read all" });
-});
+  questRouter.get("/read/all", readAllQuests);
 
 // Update 
-  questRouter.patch("/update", (req, res) => {
-  res.json({ msg: "update" });
-});
+  questRouter.patch("/update", updateQuest);
 
 // Delete 
-  questRouter.delete("/delete", (req, res) => {
-  res.json({ msg: "update" });
-});
+  questRouter.delete("/delete", deleteQuest);
 
 module.exports = questRouter;
