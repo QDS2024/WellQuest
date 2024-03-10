37c6d7df41c6034a8c486acde2cfb0aebc19b84b
kirayoon <kirayoon99@gmail.com>
2024-03-10 00:25:30 -0800

    add temp help component screen

--- client/components/profile/ProfileStack.jsx
+++ client/components/profile/ProfileStack.jsx
@@ -4,6 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import ProfileScreen from "./ProfileScreen";
 import EditInfo from "./EditInfo";
 import UserSettings from "./UserSettings";
+import Help from "./Help";
 
 const Stack = createStackNavigator();
 
@@ -16,6 +17,7 @@ export default function ProfileStack() {
       <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
       <Stack.Screen name="EditInfo" component={EditInfo} />
       <Stack.Screen name="UserSettings" component={UserSettings} />
+      <Stack.Screen name="Help" component={Help} />
     </Stack.Navigator>
   );
 }
