038e5fa6dad18747ecbc8fde29042424eb9d912e
kirayoon <kirayoon99@gmail.com>
2024-03-09 16:38:09 -0800

    move nav container to app

--- client/App.js
+++ client/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
 import Navbar from "./components/Navbar";
+import Colors from "./colors";
+
+const CustomTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: Colors.black,
+    background: Colors.white,
+  },
+};
 
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
-      <Navbar />
+      <NavigationContainer theme={CustomTheme}>
+        <Navbar />
+      </NavigationContainer>
     </View>
   );
 }
