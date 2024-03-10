c1a8f6b5645f5dd7cc413cc85fc40a5b925113d2
Zhi Li <lizhi.2496@gmail.com>
2024-03-09 17:37:12 -0800

    make changes to authentication files

--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -1,11 +1,9 @@
-const { Router } = require('express');
-const authController = require('../controllers/authController');
+const express = require('express');
+const { register, login } = require('../controllers/authController');
 
-const router = Router();
+const authRouter = express.Router();
 
-router.get('/signup', authController.signup_get);
-router.post('/signup', authController.signup_post);
-router.get('/login', authController.login_get);
-router.post('/login', authController.login_post);
+authRouter.post('/register', register);
+authRouter.post('/login', login);
 
-module.exports = router;
+module.exports = authRouter;
