8c6eed4a2bd5c9386c782c4dd62b4dc959d7bcca
kirayoon <kirayoon99@gmail.com>
2024-03-09 19:32:16 -0800

    self invoke apiUrl.js to ensure synchronous initialization

--- client/apiUrl.js
+++ client/apiUrl.js
@@ -1,7 +1,9 @@
 import { HOST } from "@env";
 let apiUrl;
-const test = "http://localhost:3000/api/user/read?id=65ece8ca4b6c918715c69896";
-const test2 = `http://${HOST}/api/`;
-apiUrl = test2;
+(() => {
+  // const test = "http://localhost:3000/api/user/read?id=65ece8ca4b6c918715c69896";
+  const test = `http://${HOST}/api/`;
+  apiUrl = test;
+})();
 
 export default apiUrl;
